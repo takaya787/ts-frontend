9c328fe2b55d0e916e2c2117fb2b703c09e26777
takaya787 <takaya318318@gmail.com>
2021-03-13 12:14:55 +0900

    modalのformの設計だけ完了

--- app/next.config.js
+++ app/next.config.js
@@ -2,3 +2,11 @@ const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true,
 })
+
+module.exports = {
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+    BASE_URL: process.env.BASE_URL,
+  }
+}
