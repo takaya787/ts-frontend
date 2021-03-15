789659e65784c7f784d504d5c79d5e227cee06de
takaya787 <takaya318318@gmail.com>
2021-03-15 18:14:43 +0900

    center pinでProvider設置

--- app/components/maps/CenterPin.tsx
+++ app/components/maps/CenterPin.tsx
@@ -1,16 +1,33 @@
-import { useEffect, useState, useContext } from 'react';
-//context
+import { useEffect, useState, createContext, Dispatch, SetStateAction } from 'react';
 //types
 import { MapCenterType } from '../../types/MapType';
 //others
 import styles from './CenterPin.module.scss';
 
+//contextを作成
+type Center_Context_Value = {
+  center: MapCenterType,
+  setCenter: Dispatch<SetStateAction<MapCenterType>>
+}
+export const CenterContext = createContext({} as Center_Context_Value)
+
 export const CenterPin: React.FC<any> = ({ $geoService }) => {
+  //投稿作成時の位置情報は,このcenterを使用する
   const [center, setCenter] = useState<MapCenterType>()
+  const CenterValue = {
+    center, setCenter
+  }
+
   const mapCenter: MapCenterType = $geoService.transform_.center;
+
+  //中心地が変わるごとにcenterの値を更新
   useEffect(function () {
     setCenter(mapCenter)
     // console.log(mapCenter)
   }, [mapCenter])
-  return (<div className={styles.marker} ></div>)
+  return (
+    <CenterContext.Provider value={CenterValue}>
+      <div className={styles.marker} ></div>
+    </CenterContext.Provider>
+  )
 }
--- app/components/maps/Map.tsx
+++ app/components/maps/Map.tsx
@@ -11,25 +11,9 @@ import styles from './Map.module.scss';
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
-//contextを作成
-
-// type Center_Context_Value = {
-//   center: MapCenterType,
-//   setCenter: Dispatch<SetStateAction<MapCenterType>>
-// }
-// export const CenterContext = createContext({} as Center_Context_Value)
-
-type Zoom_Context_Value = {
-  zoom: number,
-  setZoom: Dispatch<SetStateAction<number>>
-}
-export const ZoomContext = createContext({} as Zoom_Context_Value)
-
 export const Map: React.FC = () => {
   const InitialCenter = { lat: 48.856614, lng: 2.3522219 };
-  // const [center, setCenter] = useState<MapCenterType>(initialcenter);
-  // //Contextでの共有用
-  // const CenterValue = { center, setCenter };
+  const [Mapcenter, setMapCenter] = useState<MapCenterType>(InitialCenter);
 
   const [zoom, setZoom] = useState<number>(6.0);
   const ZoomValue = { zoom, setZoom }
@@ -41,19 +25,17 @@ export const Map: React.FC = () => {
   return (
     <div className={styles.Googlemap}>
       {/* <CenterContext.Provider value={CenterValue}> */}
-      <ZoomContext.Provider value={ZoomValue}>
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            //API_KEYは絶対に直接入力しない　過去のものは変更済み
-            key: API_KEY
-          }}
-          //defaultCenter・defaultZoomは値が固定されるので避けるべき
-          defaultCenter={InitialCenter}
-          zoom={zoom}
-        >
-          <CenterPin />
-        </GoogleMapReact>
-      </ZoomContext.Provider>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          //API_KEYは絶対に直接入力しない　過去のものは変更済み
+          key: API_KEY
+        }}
+        //defaultCenter・defaultZoomは値が固定されるので避けるべき
+        center={Mapcenter}
+        zoom={zoom}
+      >
+        <CenterPin />
+      </GoogleMapReact>
       {/* </CenterContext.Provider> */}
     </div>
   )
